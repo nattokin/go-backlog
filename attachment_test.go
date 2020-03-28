709d276774c173af598d6336d74b07ce4c4e04dd
nattokinxxxx <nattokinxxxx@gmail.com>
2020-03-29 01:59:22 +0900

    refactoring attachment

---
+++ attachment_test.go
@@ -0,0 +1,27 @@
+package backlog_test
+
+import "testing"
+
+func TestAttachmentService_Uploade(t *testing.T) {
+	
+}
+
+func TestWikiAttachmentService_Attach(t *testing.T) {
+	
+}
+
+func TestWikiAttachmentService_List(t *testing.T) {
+	
+}
+
+func TestWikiAttachmentService_Remove(t *testing.T) {
+	
+}
+
+func TestIssueAttachmentService_List(t *testing.T) {
+	
+}
+
+func TestIssueAttachmentService_Remove(t *testing.T) {
+	
+}
