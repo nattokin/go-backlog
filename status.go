83a937a8a31b44f788af202fcc4d5e292c0c2510
nattokinxxxx <nattokinxxxx@gmail.com>
2020-10-24 18:14:57 +0900

    service.go

--- status.go
+++ status.go
@@ -1,12 +1,1 @@
 package backlog
-
-// StatusService has methods for Status.
-type StatusService struct {
-	clientMethod *clientMethod
-}
-
-func newStatusService(cm *clientMethod) *StatusService {
-	return &StatusService{
-		clientMethod: cm,
-	}
-}
--- version.go
+++ version.go
@@ -1,12 +1,1 @@
 package backlog
-
-// VersionService has methods for Version.
-type VersionService struct {
-	clientMethod *clientMethod
-}
-
-func newVersionService(cm *clientMethod) *VersionService {
-	return &VersionService{
-		clientMethod: cm,
-	}
-}
