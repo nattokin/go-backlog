b3562da8342bfeeffbaf5938aeed9bc2a299123d
hogehoge <nattokinxxxx@gmail.com>
2026-03-30 02:39:07 +0900

    fix

--- project_test.go
+++ project_test.go
@@ -235,6 +235,331 @@ func TestProjectService_One(t *testing.T) {
 	}
 }
 
+func TestProjectService_Create(t *testing.T) {
+	o := newProjectOptionService()
+
+	cases := map[string]struct {
+		key     string
+		name    string
+		options []*FormOption
+
+		mockPostFn func(spath string, form *FormParams) (*http.Response, error)
+
+		wantErrType error
+	}{
+		"success-basic-create": {
+			key:  "TEST",
+			name: "test",
+
+			mockPostFn: func(spath string, form *FormParams) (*http.Response, error) {
+				assert.Equal(t, "projects", spath)
+				assert.NotNil(t, form)
+				assert.Equal(t, "TEST", form.Get("key"))
+				assert.Equal(t, "test", form.Get("name"))
+
+				return &http.Response{
+					StatusCode: http.StatusOK,
+					Body:       io.NopCloser(bytes.NewReader([]byte(testdataProjectJSON))),
+				}, nil
+			},
+
+			wantErrType: nil,
+		},
+
+		"success-without-option": {
+			key:     "TEST",
+			name:    "test",
+			options: []*FormOption{},
+
+			mockPostFn: func(spath string, form *FormParams) (*http.Response, error) {
+				assert.Equal(t, "", form.Get("chartEnabled"))
+				assert.Equal(t, "", form.Get("subtaskingEnabled"))
+				assert.Equal(t, "", form.Get("projectLeaderCanEditProjectLeader"))
+				assert.Equal(t, "", form.Get("textFormattingRule"))
+
+				return &http.Response{
+					StatusCode: http.StatusOK,
+					Body:       io.NopCloser(bytes.NewReader([]byte(testdataProjectJSON))),
+				}, nil
+			},
+
+			wantErrType: nil,
+		},
+
+		"success-with-valid-option": {
+			key:  "TEST",
+			name: "test",
+
+			options: []*FormOption{
+				o.WithFormChartEnabled(true),
+				o.WithFormSubtaskingEnabled(true),
+				o.WithFormProjectLeaderCanEditProjectLeader(true),
+				o.WithFormTextFormattingRule(FormatBacklog),
+			},
+
+			mockPostFn: func(spath string, form *FormParams) (*http.Response, error) {
+				assert.Equal(t, "true", form.Get("chartEnabled"))
+				assert.Equal(t, "true", form.Get("subtaskingEnabled"))
+				assert.Equal(t, "true", form.Get("projectLeaderCanEditProjectLeader"))
+				assert.Equal(t, "backlog", form.Get("textFormattingRule"))
+
+				return &http.Response{
+					StatusCode: http.StatusOK,
+					Body:       io.NopCloser(bytes.NewReader([]byte(testdataProjectJSON))),
+				}, nil
+			},
+
+			wantErrType: nil,
+		},
+
+		"error-empty-key": {
+			key:  "",
+			name: "test",
+
+			wantErrType: &ValidationError{},
+		},
+
+		"error-empty-name": {
+			key:  "TEST",
+			name: "",
+
+			wantErrType: &ValidationError{},
+		},
+
+		"error-option-validation": {
+			key:  "TEST",
+			name: "test",
+
+			options: []*FormOption{
+				o.WithFormTextFormattingRule("invalid"),
+			},
+
+			wantErrType: &ValidationError{},
+		},
+
+		"error-invalid-option": {
+			key:  "TEST",
+			name: "test",
+
+			options: []*FormOption{{0, nil, func(p *FormParams) error { return nil }}},
+
+			wantErrType: &InvalidFormOptionError{},
+		},
+
+		"error-client-failure": {
+			key:  "TEST",
+			name: "test",
+
+			mockPostFn: func(spath string, form *FormParams) (*http.Response, error) {
+				return nil, errors.New("error")
+			},
+
+			wantErrType: errors.New(""),
+		},
+
+		"error-invalid-json": {
+			key:  "TEST",
+			name: "test",
+
+			mockPostFn: func(spath string, form *FormParams) (*http.Response, error) {
+				return &http.Response{
+					StatusCode: http.StatusOK,
+					Body:       io.NopCloser(bytes.NewReader([]byte(testdataInvalidJSON))),
+				}, nil
+			},
+
+			wantErrType: &json.SyntaxError{},
+		},
+	}
+
+	for name, tc := range cases {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			s := newProjectService()
+
+			// default: unexpected API call
+			s.method.Post = newUnexpectedPostFn(t)
+
+			if tc.mockPostFn != nil {
+				s.method.Post = tc.mockPostFn
+			}
+
+			project, err := s.Create(tc.key, tc.name, tc.options...)
+
+			if tc.wantErrType != nil {
+				require.Error(t, err)
+				assert.IsType(t, tc.wantErrType, err)
+				assert.Nil(t, project)
+				return
+			}
+
+			assert.NoError(t, err)
+			require.NotNil(t, project)
+
+			assert.Equal(t, tc.key, project.ProjectKey)
+			assert.Equal(t, tc.name, project.Name)
+		})
+	}
+}
+
+func TestProjectService_Update(t *testing.T) {
+	o := newProjectOptionService()
+
+	cases := map[string]struct {
+		projectIDOrKey string
+		options        []*FormOption
+
+		mockPatchFn func(spath string, form *FormParams) (*http.Response, error)
+
+		wantErrType error
+	}{
+		"success-basic": {
+			projectIDOrKey: "TEST",
+
+			mockPatchFn: func(spath string, form *FormParams) (*http.Response, error) {
+				assert.Equal(t, "projects/TEST", spath)
+				assert.NotNil(t, form)
+
+				return &http.Response{
+					StatusCode: http.StatusOK,
+					Body:       io.NopCloser(bytes.NewReader([]byte(testdataProjectJSON))),
+				}, nil
+			},
+
+			wantErrType: nil,
+		},
+
+		"success-project-id": {
+			projectIDOrKey: "1234",
+
+			mockPatchFn: func(spath string, form *FormParams) (*http.Response, error) {
+				assert.Equal(t, "projects/1234", spath)
+
+				return &http.Response{
+					StatusCode: http.StatusOK,
+					Body:       io.NopCloser(bytes.NewReader([]byte(testdataProjectJSON))),
+				}, nil
+			},
+
+			wantErrType: nil,
+		},
+
+		"error-empty-key": {
+			projectIDOrKey: "",
+
+			wantErrType: &ValidationError{},
+		},
+
+		"error-zero-id": {
+			projectIDOrKey: "0",
+
+			wantErrType: &ValidationError{},
+		},
+
+		"success-with-options": {
+			projectIDOrKey: "TEST",
+
+			options: []*FormOption{
+				o.WithFormKey("TEST1"),
+				o.WithFormName("test1"),
+				o.WithFormChartEnabled(true),
+				o.WithFormSubtaskingEnabled(true),
+				o.WithFormProjectLeaderCanEditProjectLeader(true),
+				o.WithFormTextFormattingRule(FormatBacklog),
+				o.WithFormArchived(true),
+			},
+
+			mockPatchFn: func(spath string, form *FormParams) (*http.Response, error) {
+				assert.Equal(t, "TEST1", form.Get("key"))
+				assert.Equal(t, "test1", form.Get("name"))
+				assert.Equal(t, "true", form.Get("chartEnabled"))
+				assert.Equal(t, "true", form.Get("subtaskingEnabled"))
+				assert.Equal(t, "true", form.Get("projectLeaderCanEditProjectLeader"))
+				assert.Equal(t, "backlog", form.Get("textFormattingRule"))
+				assert.Equal(t, "true", form.Get("archived"))
+
+				return &http.Response{
+					StatusCode: http.StatusOK,
+					Body:       io.NopCloser(bytes.NewReader([]byte(testdataProjectJSON))),
+				}, nil
+			},
+
+			wantErrType: nil,
+		},
+
+		"error-option-validation": {
+			projectIDOrKey: "TEST",
+
+			options: []*FormOption{
+				o.WithFormTextFormattingRule("invalid"),
+			},
+
+			wantErrType: &ValidationError{},
+		},
+
+		"error-invalid-option": {
+			projectIDOrKey: "TEST",
+
+			options: []*FormOption{{0, nil, func(p *FormParams) error { return nil }}},
+
+			wantErrType: &InvalidFormOptionError{},
+		},
+
+		"error-client-failure": {
+			projectIDOrKey: "TEST",
+
+			mockPatchFn: func(spath string, form *FormParams) (*http.Response, error) {
+				return nil, errors.New("error")
+			},
+
+			wantErrType: errors.New(""),
+		},
+
+		"error-invalid-json": {
+			projectIDOrKey: "TEST",
+
+			mockPatchFn: func(spath string, form *FormParams) (*http.Response, error) {
+				return &http.Response{
+					StatusCode: http.StatusOK,
+					Body:       io.NopCloser(bytes.NewReader([]byte(testdataInvalidJSON))),
+				}, nil
+			},
+
+			wantErrType: &json.SyntaxError{},
+		},
+	}
+
+	for name, tc := range cases {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			s := newProjectService()
+
+			// default: unexpected API call
+			s.method.Patch = newUnexpectedPatchFn(t)
+
+			if tc.mockPatchFn != nil {
+				s.method.Patch = tc.mockPatchFn
+			}
+
+			project, err := s.Update(tc.projectIDOrKey, tc.options...)
+
+			if tc.wantErrType != nil {
+				require.Error(t, err)
+				assert.IsType(t, tc.wantErrType, err)
+				assert.Nil(t, project)
+				return
+			}
+
+			assert.NoError(t, err)
+			require.NotNil(t, project)
+		})
+	}
+}
+
 func TestProjectService_Delete(t *testing.T) {
 	cases := map[string]struct {
 		projectIDOrKey string
