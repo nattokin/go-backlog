c9e81718e6c573b18b7b2ea949c147f55804f3bf
nattokinxxxx <39667252+nattokin@users.noreply.github.com>
2026-04-18 18:55:19 +0900

    docs: add ExampleSpaceActivityService_List to example_space_test.go

--- example_space_test.go
+++ example_space_test.go
@@ -10,9 +10,25 @@ import (
 )
 
 var (
+	// SpaceActivityService
+	doerSpaceActivityList = newMockDoer(fixture.Activity.ListJSON)
+
 	// SpaceAttachmentService
 	doerSpaceAttachmentUpload = newMockDoer(fixture.Attachment.UploadJSON)
 )
+
+func ExampleSpaceActivityService_List() {
+	c, _ := backlog.NewClient(
+		"https://example.backlog.com",
+		"token",
+		backlog.WithDoer(doerSpaceActivityList),
+	)
+
+	activities, _ := c.Space.Activity.List(context.Background())
+	fmt.Printf("ID: %d, Type: %d\n", activities[0].ID, activities[0].Type)
+	// Output:
+	// ID: 3153, Type: 2
+}
 
 func ExampleSpaceAttachmentService_Upload() {
 	c, _ := backlog.NewClient(
