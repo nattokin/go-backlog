3f195f0cfaa53a5ac9bbc467aa8e619cede372ac
nattokinxxxx <39667252+nattokin@users.noreply.github.com>
2026-04-18 23:18:24 +0900

    test: add TestIssueOptionService to issue_test.go

--- issue_test.go
+++ issue_test.go
@@ -8,11 +8,13 @@ import (
 	"net/http"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	backlog "github.com/nattokin/go-backlog"
+	"github.com/nattokin/go-backlog/internal/core"
 	"github.com/nattokin/go-backlog/internal/testutil/fixture"
 )
 
@@ -163,6 +165,56 @@ func TestIssueAttachmentService(t *testing.T) {
 			c, err := backlog.NewClient("https://example.backlog.com", "token", backlog.WithDoer(&mockDoer{do: tc.doFunc}))
 			require.NoError(t, err)
 			tc.call(t, c)
+		})
+	}
+}
+
+func TestIssueOptionService(t *testing.T) {
+	c, err := backlog.NewClient("https://example.backlog.com", "token")
+	require.NoError(t, err)
+	s := c.Issue.Option
+
+	date := time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC)
+
+	cases := map[string]struct {
+		option  backlog.RequestOption
+		wantKey string
+	}{
+		"WithProjectIDs":      {option: s.WithProjectIDs([]int{1}), wantKey: core.ParamProjectIDs.Value()},
+		"WithIssueTypeIDs":    {option: s.WithIssueTypeIDs([]int{1}), wantKey: core.ParamIssueTypeIDs.Value()},
+		"WithCategoryIDs":     {option: s.WithCategoryIDs([]int{1}), wantKey: core.ParamCategoryIDs.Value()},
+		"WithVersionIDs":      {option: s.WithVersionIDs([]int{1}), wantKey: core.ParamVersionIDs.Value()},
+		"WithMilestoneIDs":    {option: s.WithMilestoneIDs([]int{1}), wantKey: core.ParamMilestoneIDs.Value()},
+		"WithStatusIDs":       {option: s.WithStatusIDs([]int{1}), wantKey: core.ParamStatusIDs.Value()},
+		"WithPriorityIDs":     {option: s.WithPriorityIDs([]int{1}), wantKey: core.ParamPriorityIDs.Value()},
+		"WithAssigneeIDs":     {option: s.WithAssigneeIDs([]int{1}), wantKey: core.ParamAssigneeIDs.Value()},
+		"WithCreatedUserIDs":  {option: s.WithCreatedUserIDs([]int{1}), wantKey: core.ParamCreatedUserIDs.Value()},
+		"WithResolutionIDs":   {option: s.WithResolutionIDs([]int{1}), wantKey: core.ParamResolutionIDs.Value()},
+		"WithParentChild":     {option: s.WithParentChild(0), wantKey: core.ParamParentChild.Value()},
+		"WithAttachment":      {option: s.WithAttachment(true), wantKey: core.ParamAttachment.Value()},
+		"WithSharedFile":      {option: s.WithSharedFile(true), wantKey: core.ParamSharedFile.Value()},
+		"WithIssueSort":       {option: s.WithIssueSort(backlog.IssueSortCreated), wantKey: core.ParamSort.Value()},
+		"WithOrder":           {option: s.WithOrder(backlog.OrderAsc), wantKey: core.ParamOrder.Value()},
+		"WithOffset":          {option: s.WithOffset(0), wantKey: core.ParamOffset.Value()},
+		"WithCount":           {option: s.WithCount(20), wantKey: core.ParamCount.Value()},
+		"WithCreatedSince":    {option: s.WithCreatedSince(date), wantKey: core.ParamCreatedSince.Value()},
+		"WithCreatedUntil":    {option: s.WithCreatedUntil(date), wantKey: core.ParamCreatedUntil.Value()},
+		"WithUpdatedSince":    {option: s.WithUpdatedSince(date), wantKey: core.ParamUpdatedSince.Value()},
+		"WithUpdatedUntil":    {option: s.WithUpdatedUntil(date), wantKey: core.ParamUpdatedUntil.Value()},
+		"WithStartDateSince":  {option: s.WithStartDateSince(date), wantKey: core.ParamStartDateSince.Value()},
+		"WithStartDateUntil":  {option: s.WithStartDateUntil(date), wantKey: core.ParamStartDateUntil.Value()},
+		"WithDueDateSince":    {option: s.WithDueDateSince(date), wantKey: core.ParamDueDateSince.Value()},
+		"WithDueDateUntil":    {option: s.WithDueDateUntil(date), wantKey: core.ParamDueDateUntil.Value()},
+		"WithHasDueDate":      {option: s.WithHasDueDate(false), wantKey: core.ParamHasDueDate.Value()},
+		"WithIDs":             {option: s.WithIDs([]int{1}), wantKey: core.ParamIDs.Value()},
+		"WithParentIssueIDs":  {option: s.WithParentIssueIDs([]int{1}), wantKey: core.ParamParentIssueIDs.Value()},
+		"WithKeyword":         {option: s.WithKeyword("bug"), wantKey: core.ParamKeyword.Value()},
+	}
+
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			assert.Equal(t, tc.wantKey, tc.option.Key())
 		})
 	}
 }
