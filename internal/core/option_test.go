13575a3731ff9ff2e2c9e05657c3716f8422f8b9
hogehoge <nattokinxxxx@gmail.com>
2026-04-24 21:33:59 +0900

    fix

--- internal/core/option_test.go
+++ internal/core/option_test.go
@@ -5,9 +5,10 @@ import (
 	"net/url"
 	"testing"
 
-	"github.com/nattokin/go-backlog/internal/core"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/nattokin/go-backlog/internal/core"
 )
 
 func TestAPIParamOption(t *testing.T) {
