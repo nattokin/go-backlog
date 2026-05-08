e4919d6612963a54835f05f38c791261bd96b1e7
hogehoge <nattokinxxxx@gmail.com>
2026-05-08 19:49:25 +0900

    fix

--- internal/user/service_project_test.go
+++
@@ -1,4 +0,0 @@
-// This file is intentionally empty.
-// ProjectUserService has been moved to internal/project/service_user.go.
-// Tests are in internal/project/service_user_test.go.
-package user_test
