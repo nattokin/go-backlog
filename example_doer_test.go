b48426cec3e59d79fb360cb247bb424d648c32b1
hogehoge <nattokinxxxx@gmail.com>
2026-05-21 02:15:24 +0900

    fix

--- example_doer_test.go
+++ example_doer_test.go
@@ -1,10 +1,6 @@
 package backlog_test
 
 import (
-	"bytes"
-	"io"
-	"net/http"
-
 	"github.com/nattokin/go-backlog/internal/testutil/fixture"
 	"github.com/nattokin/go-backlog/internal/testutil/mock"
 )
@@ -108,9 +104,7 @@ var (
 // newMockDoer returns a mockDoer that always responds with HTTP 200 and the given body.
 func newMockDoer(json string) *mock.Doer {
 	return &mock.Doer{
-		DoFunc: func(_ *http.Request) (*http.Response, error) {
-			return mock.NewJSONResponse(json), nil
-		},
+		DoFunc: mock.NewJSONDoFunc(json),
 	}
 }
 
@@ -118,22 +112,11 @@ func newMockDoer(json string) *mock.Doer {
 // and the given binary body, filename, and Content-Type.
 func newMockBinaryDoer(contentType, filename string, body []byte) *mock.Doer {
 	return &mock.Doer{
-		DoFunc: func(_ *http.Request) (*http.Response, error) {
-			return &http.Response{
-				StatusCode: http.StatusOK,
-				Header: http.Header{
-					"Content-Type":        []string{contentType},
-					"Content-Disposition": []string{"attachment; filename=" + filename},
-				},
-				Body: io.NopCloser(bytes.NewReader(body)),
-			}, nil
-		},
+		DoFunc: mock.NewBinaryDoFunc(filename, contentType, body),
 	}
 }
 
 // doerNoContent is a mockDoer that always responds with HTTP 204 No Content.
 var doerNoContent = &mock.Doer{
-	DoFunc: func(_ *http.Request) (*http.Response, error) {
-		return &http.Response{StatusCode: http.StatusNoContent, Body: http.NoBody}, nil
-	},
+	DoFunc: mock.NewNoContentDoFunc(),
 }
